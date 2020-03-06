a1e1118fa801ae1c32c837c38cbec23538a5b04d
LEEHANSOO <rkrk6469@nate.com>
2020-03-06 13:26:44 +0900

    no message

---
+++ src/main/java/com/android/music/MusicAndApplication.java
@@ -0,0 +1,13 @@
+package com.android.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicAndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/android/music/MusicAndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.android.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
